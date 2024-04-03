cc20a9511f0e7caa1f2154700c0f55f3f75d8a9a
santibrizuela <santibrizuela.97@gmail.com>
2024-04-03 13:28:38 -0300

    ProductDetail, Ratings

---
+++ app/product/[productId]/page.tsx
@@ -0,0 +1,24 @@
+import { product } from "@/utils/product";
+import ProductDetails from "./ProductDetails";
+import Container from "../../components/Container";
+import ListRating from "./ListRating";
+
+interface IPrams {
+    productId?: string;
+}
+
+const Product = ({ params }: { params: IPrams }) => {
+    console.log('params', params)
+    
+    return ( <div className="p-8">
+        <Container>
+            <ProductDetails product={product}/>
+            <div className="flex flex-col mt-20 gap-4">
+                <div>Add Rating</div>
+                <ListRating product={product} />
+            </div>
+        </Container>
+    </div> );
+}
+ 
+export default Product;
