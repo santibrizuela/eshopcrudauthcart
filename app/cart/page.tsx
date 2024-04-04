4b1023c8302a34632a3a21ed53c96393f3e39707
santibrizuela <santibrizuela.97@gmail.com>
2024-04-04 19:42:08 -0300

    Cart UI

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,12 @@
+import Container from "../components/Container";
+import CartClient from "./CartClient";
+
 const Cart = () => {
-    return ( <div>Cart PAge</div> );
+    return ( <div className="pt-8">
+        <Container>
+            <CartClient/>
+        </Container>
+    </div> );
 }
  
 export default Cart;
