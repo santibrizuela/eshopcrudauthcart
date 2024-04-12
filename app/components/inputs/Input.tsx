2735e73913665bdd65315cbd6f34312d9a2a1511
santibrizuela <santibrizuela.97@gmail.com>
2024-04-12 20:56:53 -0300

    nextauth finish

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -37,7 +37,10 @@ const Input: React.FC<InputProps> = ({
             />
             <label 
                 htmlFor={id}
-                className="absolute cursor-text text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] left-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:translate-y-4"    
+                className={`
+                    absolute cursor-text text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] left-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4
+                    ${errors[id] ? 'text-rose-500' : 'text-slate-400'}
+                `}    
             >{label}</label>
         </div>
     );
